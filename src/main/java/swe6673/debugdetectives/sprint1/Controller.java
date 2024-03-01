53d9bc72fbb771884fc14a7eae501fbdd6fd567e
Jeffrey Scoggins <JeffreyScoggins84@gmail.com>
2024-02-29 21:16:58 -0500

    added Tests in Controller Class

--- src/main/java/swe6673/debugdetectives/sprint1/Controller.java
+++ src/main/java/swe6673/debugdetectives/sprint1/Controller.java
@@ -7,6 +7,10 @@ public class Controller {
     @FXML
     private Label welcomeText;
 
+    String onScreenText = "";
+    String currentEquation = "";
+    String currentInput = "";
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
@@ -15,7 +19,40 @@ public class Controller {
     int maximumNumberLength(){
         return 10;
     }
-    boolean checkNumberLength(double number){
+    boolean checkNumberLength(double number) throws Exception{
         return true;
     }
+
+    double convertIntToDouble(int num) throws Exception{
+        return 0.0;
+    }
+
+    String convertNumToString(int num) throws Exception{
+        return "";
+    }
+
+    String convertNumToString(double num) throws Exception{
+        return "";
+    }
+
+    void setEquation(String input)throws Exception{
+        this.currentEquation = input;
+    }
+
+    public void equals(){
+
+    }
+    public void clearScreen(){
+
+    }
+
+    double shortenDecimal(double number){ //shortens decimals and rounds up if 11th decimal is >= 5
+        return number;
+    }
+
+    String shortenLong(double number){ //returns scientific notation on numbers larger than 10 whole digits
+        String sciNotation = "";
+        return sciNotation;
+    }
+
 }
--- src/test/java/swe6673/debugdetectives/sprint1/ControllerTest.java
+++ src/test/java/swe6673/debugdetectives/sprint1/ControllerTest.java
@@ -11,7 +11,7 @@ import static org.junit.Before.*;
 public class ControllerTest {
 
     @Test
-    public void checkNumberLengthFalse(){
+    public void checkNumberLengthFalse() throws Exception {
         Controller numberLength = new Controller();
         //purpose to ensure that maximum number of digits cannot be exceeded
         boolean actual = numberLength.checkNumberLength(10000000000.0); //10,000,000,000 eleven digits
@@ -19,7 +19,7 @@ public class ControllerTest {
         assertEquals("Error, test results are different than expected", expected, actual);
     }
     @Test
-    public void checkNumberLengthTrue(){
+    public void checkNumberLengthTrue() throws Exception {
         Controller numberLength = new Controller();
         //purpose to ensure that maximum number of digits cannot be exceeded
         boolean actual = numberLength.checkNumberLength(100000000.0); //10,000,000,000 eleven digits
@@ -28,7 +28,7 @@ public class ControllerTest {
     }
 
     @Test
-    public void checkDecimalLengthFalse(){
+    public void checkDecimalLengthFalse() throws Exception {
         Controller numberLength = new Controller();
         //purpose to ensure that maximum number of digits cannot be exceeded
         boolean actual = numberLength.checkNumberLength(0.0000000008); // ten decimals and one whole number
@@ -37,12 +37,121 @@ public class ControllerTest {
     }
 
     @Test
-    public void checkDecimalLengthTrue(){
+    public void checkDecimalLengthTrue() throws Exception {
         Controller numberLength = new Controller();
         //purpose to ensure that maximum number of digits cannot be exceeded
         boolean actual = numberLength.checkNumberLength(9.000000008); // ten decimals and one whole number
         boolean expected = true;
         assertEquals("Error, test results are different than expected", expected, actual);
     }
+
+    @Test
+    public void covertIntToDoublePositive() throws Exception {
+        //purpose to check to see if Intergers can be converted to Doubles if needed
+        Controller number = new Controller();
+        double actual = number.convertIntToDouble(36);
+        double expected = 36.0;
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void covertIntToDoubleNegative() throws Exception {
+        //purpose to check to see if Intergers can be converted to Doubles if needed
+        Controller number = new Controller();
+        double actual = number.convertIntToDouble(-23);
+        double expected = -23.0;
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void covertIntToDouble() throws Exception {
+        //purpose to check to see if Intergers can be converted to Doubles if needed
+        Controller number = new Controller();
+        double actual = number.convertIntToDouble(10);
+        int expected = 10;
+        assertNotEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void clearScreen() throws Exception {
+        //purpose to check to see if screen can be cleared
+        Controller clear = new Controller();
+        String actual = clear.onScreenText;
+        String expected = "";
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void convertDoubleToString() throws Exception {
+        //purpose to check to ensure doubles are converted to string
+        Controller dub = new Controller();
+        String actual = dub.convertNumToString(44.0);
+        String expected = "44";
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void convertIntToString() throws Exception {
+        //purpose to check to ensure doubles are converted to string
+        Controller int_ = new Controller();
+        String actual = int_.convertNumToString(44);
+        String expected = "44";
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void storeInput() throws Exception {
+        //purpose to ensure that on screen text is being properly stored once a modifier key is pressed
+        Controller num = new Controller();
+        num.setEquation("630*3+99");
+        String actual = num.currentEquation;
+        String expected = "630*3+99";
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void equalsButton() throws Exception {
+        //purpose to check to ensure doubles are converted to string
+        Controller equation = new Controller();
+        /*Add test!!!
+        Add test!!!
+        Add test!!!
+        Add test!!!
+        Add test!!!
+        Add test!!!
+         */
+        String actual = "";
+        String expected = "";
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void shortenDecimalWithRound() throws Exception {
+        //purpose to ensure that number is shorter than max number of characters on screen and rounds up if 11th number =>5
+        Controller num = new Controller();
+        Double actual = num.shortenDecimal(38.123456789);
+        Double expected = 38.12345679;
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void shortenDecimalNoRound() throws Exception {
+        //purpose to ensure that number is shorter than max number of characters on screen and does not rounds up if 11th number < 5
+        Controller num = new Controller();
+        Double actual = num.shortenDecimal(38.123456781);
+        Double expected = 38.12345678;
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+    @Test
+    public void shortenLong() throws Exception {
+        //purpose to ensure that number is shorter than max number of characters on screen and rounds up if 11th number =>5
+        Controller num = new Controller();
+        String actual = num.shortenLong(90876543219999.0);
+        String expected = "9.08 x 10^14";
+        assertEquals("Error, test results are different than expected", expected, actual);
+    }
+
+
 
 }
