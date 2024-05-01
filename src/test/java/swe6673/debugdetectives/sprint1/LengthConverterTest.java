e21e537fcef19ba0c69f409ceb1e297d2a526853
Anh Duong <aduong2@students.kennesaw.edu>
2024-05-01 14:53:23 -0400

    updated LengthConverterTest and MassConverterTest

--- src/test/java/swe6673/debugdetectives/sprint1/LengthConverterTest.java
+++ src/test/java/swe6673/debugdetectives/sprint1/LengthConverterTest.java
@@ -12,66 +12,180 @@ public class LengthConverterTest {
     public void convertMeterToCentimeter(){
         //purpose: test meter to centimeter conversion
         LengthConverter convert = new LengthConverter();
+
+        //Test 1
         double expected = 1000.0;
         double actual = convert.convertFrom(10.0, "meter", "centimeter");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 100.0;
+        actual = convert.convertFrom(1.0, "meter", "centimeter");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 3
+        expected = 1.0;
+        actual = convert.convertFrom(0.01, "meter", "centimeter");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive Test
+        expected = 0.0;
+        actual = convert.convertFrom(10.0, "meter", "centimeter");
+        assertNotEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
     public void convertMeterToKilometer(){
         //purpose: test meter to kilometer
         LengthConverter convert = new LengthConverter();
+
+        //Test 1
         double expected = 0.01;
         double actual = convert.convertFrom(10.0, "meter", "kilometer");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 0.001 ;
+        actual = convert.convertFrom(1.0, "meter", "kilometer");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive Test
+        expected = 0 ;
+        actual = convert.convertFrom(0.001, "meter", "kilometer");
+        assertNotEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
     public void convertMeterToMillimeter(){
         //purpose: test meter to millimeter
         LengthConverter convert = new LengthConverter();
+
+        //Test 1
         double expected = 10000.0;
         double actual = convert.convertFrom(10.0, "meter", "millimeter");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 209.8;
+        actual = convert.convertFrom(0.2098, "meter", "millimeter");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 3
+        expected = 0.001;
+        actual = convert.convertFrom(0.000001, "meter", "millimeter");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive
+        expected = 0;
+        actual = convert.convertFrom(1234.6, "meter", "millimeter");
+        assertNotEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
     public void convertMeterToMile(){
         //purpose: test meter to mile with long decimal point output
         LengthConverter convert = new LengthConverter();
+
+        //Test 1
         double expected = 0.006213711922373339;
         double actual = convert.convertFrom(10.0, "meter", "mile");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 0.06282000616;
+        actual = convert.convertFrom(101.099, "meter", "mile");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 3
+        expected = 9999.999;
+        actual = convert.convertFrom(9999.999, "meter", "mile");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive
+        expected = 0;
+        actual = convert.convertFrom(0.01204, "meter", "mile");
+        assertNotEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
     public void convertMeterToYard(){
         //purpose: test meter to yard
         LengthConverter convert = new LengthConverter();
+
+        //Test 1
         double expected = 1.09361;
         double actual = convert.convertFrom(10.0, "meter", "yard");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 110.34558;
+        actual = convert.convertFrom(100.9, "meter", "yard");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 3
+        expected = 0.0010936133;
+        actual = convert.convertFrom(0.001, "meter", "yard");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive Test
+        expected = 0;
+        actual = convert.convertFrom(123.3, "meter", "yard");
+        assertNotEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
     }
     @Test
     public void convertMeterToFoot(){
         //purpose: test meter to foot
         LengthConverter convert = new LengthConverter();
+
+        //Test 1
         double expected = 3.28084;
         double actual = convert.convertFrom(10.0, "meter", "foot");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 327.7559;
+        actual = convert.convertFrom(99.9, "meter", "foot");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive Test
+        expected = 0;
+        actual = convert.convertFrom(0.0001, "meter", "foot");
+        assertNotEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
     public void convertMeterToInch(){
         //test meter to inch
         LengthConverter convert = new LengthConverter();
+        Formatter fmt = new Formatter();
+
+        //Test 1
         double expected = 393.701;
         double actual = convert.convertFrom(10.0, "meter", "inch");
-        Formatter fmt = new Formatter();
         fmt = fmt.format("%.3f", actual);
         assertEquals("Error, test results are different than expected", expected, Double.valueOf(String.valueOf(fmt)), 0.0003);
+
+        //Test 2
+        expected = 3933.071;
+        actual = convert.convertFrom(99.9, "meter", "inch");
+        fmt = fmt.format("%.3f", actual);
+        assertEquals("Error, test results are different than expected", expected, Double.valueOf(String.valueOf(fmt)), 0.0003);
+
+        //False Positive Test
+        expected = 0;
+        actual = convert.convertFrom(99.9, "meter", "inch");
+        fmt = fmt.format("%.3f", actual);
+        assertNotEquals("Error, test results are different than expected", expected, Double.valueOf(String.valueOf(fmt)), 0.0003);
     }
     @Test
     public void convertNegativeNumbers() {
         //purpose: test length conversion with negative input number
         LengthConverter convert = new LengthConverter();
+
+        //Test 1
         double expected = -40000.0;
         double actual = convert.convertFrom(-400.0, "meter", "centimeter");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = -0.099987;
+        actual = convert.convertFrom(-99.987, "meter", "kilometer");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
--- src/test/java/swe6673/debugdetectives/sprint1/MassConverterTest.java
+++ src/test/java/swe6673/debugdetectives/sprint1/MassConverterTest.java
@@ -3,38 +3,102 @@ package swe6673.debugdetectives.sprint1;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class MassConverterTest {
     @Test
     public void convertMetricTonToPound(){
         //purpose: test metric ton to pound with boundary value of 0.01
         MassConverter convert = new MassConverter();
+
+        //Test 1
         double expected = 22.04623;
         double actual = convert.convertFrom(0.01, "metricTon", "pound");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 2182.576;
+        actual = convert.convertFrom(0.99, "metricTon", "pound");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 3
+        expected = 0.220462262;
+        actual = convert.convertFrom(0.0001, "metricTon", "pound");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive Test
+        expected = 0;
+        actual = convert.convertFrom(18934.04, "metricTon", "pound");
+        assertNotEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
     public void convertStoneToGram(){
         //purpose: test stone to gram with small decimal value
         MassConverter convert = new MassConverter();
+
+        //Test 1
         double expected = 0.635029318;
         double actual = convert.convertFrom(0.0001, "stone", "gram");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 634965.82;
+        actual = convert.convertFrom(99.99, "stone", "gram");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive Test
+        expected = 0;
+        actual = convert.convertFrom(0.00001, "stone", "gram");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
     public void convertMilligramToMicrogram(){
         //purpose: test miligram to microgram with boundary value of 999.9
         MassConverter convert = new MassConverter();
+
+        //Test 1
         double expected = 999900;
         double actual = convert.convertFrom(999.9, "milligram", "microgram");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 1;
+        actual = convert.convertFrom(0.001, "milligram", "microgram");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 3
+        expected = 0.0023885;
+        actual = convert.convertFrom(0.0023885, "milligram", "microgram");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive Test
+        expected = 0;
+        actual = convert.convertFrom(0.0023885, "milligram", "microgram");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
     public void convertImperialTonToUSTon(){
         //purpose: test imperial ton to US ton
         MassConverter convert = new MassConverter();
+
+        //Test 1
         double expected = 1120.0;
         double actual = convert.convertFrom(1000.0, "imperialTon", "USTon");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 2
+        expected = 0.00112;
+        actual = convert.convertFrom(0.001, "imperialTon", "USTon");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //Test 3
+        expected = 111.8990096;
+        actual = convert.convertFrom(99.90983, "imperialTon", "USTon");
+        assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
+
+        //False Positive Test
+        expected = 0;
+        actual = convert.convertFrom(1.0, "imperialTon", "USTon");
         assertEquals("Error, test results are different than expected", expected, actual, 0.0003);
     }
     @Test
